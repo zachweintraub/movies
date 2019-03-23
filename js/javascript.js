dc1b7e7dfb3698a9170120bcb81119d037bc9ed4
Zach Weintraub <zachweintraub@gmail.com>
2019-03-22 17:36:28 -0700

    full functionality, missing stills from acting roles

---
+++ js/javascript.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+  $('#about').click(function() {
+    $('.selected').removeClass('selected');
+    $('#about').addClass('selected');
+    $('[id^=section]').addClass('invisible');
+    $('#section-about').removeClass('invisible');
+  })
+
+  $('#made').click(function() {
+    $('.selected').removeClass('selected');
+    $('#made').addClass('selected');
+    $('[id^=section]').addClass('invisible');
+    $('#section-made').removeClass('invisible');
+  })
+
+  $('#played').click(function() {
+    $('.selected').removeClass('selected');
+    $('#played').addClass('selected');
+    $('[id^=section]').addClass('invisible');
+    $('#section-played').removeClass('invisible');
+  })
+
+  $('#hmu').click(function() {
+    $('.selected').removeClass('selected');
+    $('#hmu').addClass('selected');
+    $('[id^=section]').addClass('invisible');
+    $('#section-hmu').removeClass('invisible');
+  })
+
+  $('#hmu2').click(function() {
+    $('.selected').removeClass('selected');
+    $('#hmu').addClass('selected');
+    $('[id^=section]').addClass('invisible');
+    $('#section-hmu').removeClass('invisible');
+  })
+
+});
